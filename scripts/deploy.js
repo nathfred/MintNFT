50724f94aeac457e071eb923dd212b9bc51c6a47
Nathanael Fredericko <79194414+nathfred@users.noreply.github.com>
2025-03-04 22:13:45 +0700

    adjustments

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,6 @@
+const hre = require("hardhat");
+const { ethers } = hre;
+
 async function main() {
   // if you changed the name of the contract, be sure to update this here!
   const MyToken = await hre.ethers.getContractFactory("MyToken");
@@ -7,11 +10,14 @@ async function main() {
   await nft.deployed();
 
   console.log("NFT deployed to:", nft.address);
- 
+
   // mint one to yourself!
   const signer0 = await ethers.provider.getSigner(0);
   // update the IPFS CID to be your metadata CID
-  await nft.safeMint(await signer0.getAddress(), "ipfs://QmbCwoJB7g9b86BfEahsjzyzzNx3WEu7Jc1opXWgeTXFgb");
+  await nft.safeMint(
+    await signer0.getAddress(),
+    "https://bafybeiaiz2dpkve7zwafmqghqd6pmwglmcxfal4rczcqcfxp6buaq6vd4y.ipfs.dweb.link?filename=Screenshot%20at%20Aug%2031%2012-02-04.png"
+  );
 
   console.log("NFT Minted!");
 }
